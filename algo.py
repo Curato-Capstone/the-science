ecddf3cd57e9cfab79a99b836235e88cab84871b
mmmscott <mmmscott@uw.edu>
2016-04-09 18:49:41 -0700

    improve algorithm, update testing script thing

--- algo.py
+++ algo.py
@@ -1,6 +1,18 @@
 import math
 from users import *
 
+# Basic test taxonomy to help remove items from suggestions
+TAXONOMY = {
+  "outdoors": ["park"],
+  "culture": ["art_gallery", "museum"],
+  "sports": [],
+  "price": [],
+  "food": ["restaurant", "food"],
+  "entertainment": [],
+  "relaxation": [],
+  "shopping": []
+}
+
 # TODO: make this rethinky
 def find_user_by_id(id):
   for user in all_users:
@@ -14,6 +26,9 @@ def find_business_by_id(bid):
     if business['business_id'] == bid:
       return business
 
+def get_all_businesses():
+  return all_businesses
+
 # TODO: make this not crappy
 def k_nearest_neighbors(person, k):
   d = dict()
@@ -26,7 +41,6 @@ def k_nearest_neighbors(person, k):
 
   return sorted(d.items(), key=lambda x: x[1])[0:k]
 
-
 # Takes two user preferences and returns distance between all dimensions
 def calc_distance(pref1, pref2):
   sum = 0
@@ -35,28 +49,86 @@ def calc_distance(pref1, pref2):
   return math.sqrt(sum)
 
 # Choose some random suggestions based on the neighbors and optional query string
-def choose_suggestions(neighbors, query=""):
+def choose_suggestions(user_id, neighbors, query=""):
+  num_sugs = 3
   temp_set = set()
   final_set = set()
+  query_set = set()
+  too_few = False
 
+  # Store all items liked by neighbors
   for neighbor in neighbors:
     user = find_user_by_id(neighbor[0])
     temp_set.update(user['liked_businesses'])
 
+  # For all items, get business info and add to the final set of businesses if there needs to be a query term
   for suggestion in temp_set:
     bus = find_business_by_id(suggestion)
     if query and query not in bus['tags']:
         continue
     final_set.add(suggestion)
 
-  num_sugs = 3 if len(final_set) >= 3 else len(final_set)
+
+  if len(final_set) >= 3:
+    final_set = filter_suggestions(user_id, final_set, query)
+  elif len(final_set) >= 0:
+    if query:
+      query_set.update(final_set)
+    too_few = True
+    final_set.update(filter_suggestions(user_id, get_all_businesses(), query))
+
+  # give preference to search term rather than their interests
+  if query and too_few:
+    for item in final_set:
+      if query in find_business_by_id(item)['tags']:
+        query_set.add(item)
+
+    if len(query_set) > 3:
+      return random.sample(query_set, 3)
+
+    query_set.add(random.sample(final_set, num_sugs - len(query_set))[0])
+    return query_set
+
   return random.sample(final_set, num_sugs)
+
+# Filters suggestions by a user's preferences and returns it
+def filter_suggestions(user_id, suggestions, query):
+  user_prefs = find_user_by_id(user_id)['preferences']
+  good_prefs = set()
+
+  # if a user likes something enough, save it
+  for pref in user_prefs:
+    if user_prefs[pref] >= 3:
+      good_prefs.add(pref)
+
+  filtered = set()
+
+  # For the cold start, if no other users exist and have no suggestions, choose
+  # places that people might like based off taxonomy
+  if type(suggestions) != set:
+    for suggestion in suggestions:
+      for pref in good_prefs:
+        intersect = set(suggestion['tags']).intersection(TAXONOMY[pref])
+        if len(intersect) !=  0 or query in suggestion['tags']:
+          filtered.add(suggestion['business_id'])
+  else:
+    for suggestion in suggestions:
+      buss = find_business_by_id(suggestion)
+
+      # Filter out suggestions if they are something that the user isn't interested in
+      for pref in good_prefs:
+        intersect = set(buss['tags']).intersection(TAXONOMY[pref])
+        if len(intersect) != 0 or query in buss['tags']:
+          filtered.add(suggestion)
+
+  return filtered
+
 
 # Called by microservice, gets all them suggestions
 def get_suggestions(user_id, k_num, query):
   user = find_user_by_id(user_id)
   neighbors = k_nearest_neighbors(user, k_num)
-  suggestions = choose_suggestions(neighbors, query)
+  suggestions = choose_suggestions(user_id, neighbors, query)
   things = []
   for sugg in suggestions:
     things.append(find_business_by_id(sugg))
--- users.py
+++ users.py
@@ -5,11 +5,13 @@ import uuid
 all_businesses = dict()
 
 yay_sets = [set(), set(), set(), set(), set(), set(), set()]
+park_set = set()
+museum_set = set()
 
 all_users = list()
 
 def user_generator(id):
-  all_users.append({
+  some_user = {
     'id': id,
     'preferences': {
       'price': random.randint(1, 5),
@@ -22,7 +24,20 @@ def user_generator(id):
       'sports': random.randint(1, 5)
     },
     'liked_businesses': random.sample(yay_sets, 1)[0]
-  })
+  }
+
+  '''if some_user['preferences']['outdoors'] == 4:
+    some_user['liked_businesses'].update(random.sample(park_set, len(park_set)))
+  elif some_user['preferences']['outdoors'] == 5:
+    some_user['liked_businesses'] = park_set
+  elif some_user['preferences']['culture'] == 4:
+    some_user['liked_businesses'].update(random.sample(museum_set, len(museum_set)))
+  elif some_user['preferences']['culture'] == 5:
+    some_user['liked_businesses'] = museum_set'''
+
+  all_users.append(some_user)
+
+best_park = ""
 
 with open("businesses.json") as json_file:
     all_businesses = json.load(json_file)
@@ -31,7 +46,62 @@ for business in all_businesses:
   business['business_id']  = uuid.uuid1()
   the_set = random.sample(yay_sets[:-1], 1)[0]
   the_set.add(business['business_id'])
+  if 'park' in business['tags']:
+    park_set.add(business['business_id'])
+    best_park = business['business_id']
+  elif 'museum' in business['tags'] or 'art_gallery' in business['tags']:
+    museum_set.add(business['business_id'])
 
 for i in range(0,10000):
   user_generator(i)
 
+loves_parks = {
+    'id': 98765,
+    'preferences': {
+      'price': 1,
+      'culture': 1,
+      'food': 1,
+      'outdoors': 5,
+      'entertainment': 1,
+      'relaxation': 1,
+      'shopping': 1,
+      'sports': 1
+    },
+    'liked_businesses': set()
+}
+
+loves_art = {
+    'id': 12345,
+    'preferences': {
+      'price': 1,
+      'culture': 5,
+      'food': 1,
+      'outdoors': 1,
+      'entertainment': 1,
+      'relaxation': 1,
+      'shopping': 1,
+      'sports': 1
+    },
+    'liked_businesses': set()
+}
+
+loves_artpark = {
+    'id': 12346,
+    'preferences': {
+      'price': 1,
+      'culture': 5,
+      'food': 1,
+      'outdoors': 1,
+      'entertainment': 1,
+      'relaxation': 1,
+      'shopping': 1,
+      'sports': 1
+    },
+    'liked_businesses': set()
+}
+
+loves_artpark['liked_businesses'].add(best_park)
+
+all_users.append(loves_parks)
+all_users.append(loves_art)
+all_users.append(loves_artpark)
